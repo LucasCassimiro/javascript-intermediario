da3951fb8cc7596452f231cba99b52dedb62c613
Lucas Cassimiro <lucas.cassimir@gmail.com>
2020-06-04 14:14:30 -0300

    Criação do novo repositorio e adição do arquivo template-string.js

---
+++ template-string.js
@@ -0,0 +1,6 @@
+let meuNome = "Lucas"
+let sobrenome = "Cassimiro"
+let minhaProfissao = "Estudante"
+
+console.log(`Meu nomé é ${meuNome} ${sobrenome}, sou ${minhaProfissao}!`);
+console.log(`O resultado da soma de 5 + 5 é = ${5+5}`);
