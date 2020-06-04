38a54a5e4a8c62afc83438ad68ad7f7d69407b8f
Lucas Cassimiro <lucas.cassimir@gmail.com>
2020-06-04 14:42:45 -0300

    Alteração do arquivo ternario, adição do arquivo arrow.js

---
+++ arrow.js
@@ -0,0 +1,18 @@
+function soma(x , y){
+    return x + y;    
+}
+
+const mutiplica = function(x , y) {
+    return x * y;    
+}
+
+console.log(mutiplica(5,5));
+
+//com arrow// 
+const divide = (x , y) => {
+    return x / y;
+}
+
+console.log(divide(6 , 2));
+
+const objeto = () => ({nome : "Lucas", sobrenome : "Cassimiro"});
