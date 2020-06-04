74f7022c0a55da7f04034229b0df2a6bab3ff93a
Lucas Cassimiro <lucas.cassimir@gmail.com>
2020-06-04 14:21:58 -0300

    Adição do arquivo ternario.js

---
+++ ternario.js
@@ -0,0 +1,20 @@
+let isValid = true;
+
+function verify(isValid){
+    if(isValid){
+        return true;
+    }
+    else{
+        return false;
+    }
+}
+console.log(verify(isValid));
+
+// Com ternário//
+const result = isValid ? true : false;
+
+let zero = 0;
+
+const numeroResultado = zero > 0 ? 0 : -1;
+
+console.log(numeroResultado);
