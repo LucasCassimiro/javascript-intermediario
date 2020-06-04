aeed767b7e7364f5eaca8f5310715d2f9b4e0834
Lucas Cassimiro <lucas.cassimir@gmail.com>
2020-06-04 18:50:22 -0300

    Adição do arquivo desestruct.js

---
+++ desestruct.js
@@ -0,0 +1,12 @@
+const pessoa = {
+    nome: "Lucas",
+    sobrenome: "Cassimiro",
+    idade: "25",
+    profissao: "Estudante"
+};
+
+console.log(pessoa);
+
+let {nome, sobrenome, idade, profissao} = pessoa;
+
+console.log(nome, sobrenome, idade, profissao);
